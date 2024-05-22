e3b170c572c4c09fa472ef8885a8c5bb8bd6eacc
guirosasantos <guirosasantos1010@gmail.com>
2024-05-22 11:38:33 -0300

    wip add threads to insert new task with pipe

---
+++ Scheduler/create_tasks.c
@@ -0,0 +1,58 @@
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "task.h"
+#include <stdlib.h>
+
+Task CreateTask()
+{
+    Task *task = (Task *)malloc(sizeof(Task));
+    
+    char* name = (char *)malloc(100);
+    int tid, priority, burst, deadline;
+
+    printf("Enter task name: ");
+    scanf("%s", name);
+    task->name = name;
+    printf("Enter task tid: ");
+    scanf("%d", &task->tid);
+    printf("Enter task priority: ");
+    scanf("%d", &task->priority);
+    printf("Enter task burst: ");
+    scanf("%d", &task->burst);
+    printf("Enter task deadline: ");
+    scanf("%d", &task->deadline);
+    return *task;
+}
+
+int main()
+{
+    int file;
+    char *pipefile = "/tmp/pipefile";
+
+    printf("Creating tasks\n");
+    mkfifo(pipefile, 0666);
+    file = open(pipefile, O_WRONLY);
+    printf("Pipe created\n");
+
+    while (1)
+    {
+        Task task = CreateTask();
+        printf("\n Sending task %s with priority %d to the scheduler\n", task.name, task.priority);
+        write(file, task.name, strlen(task.name) + 1); // send the name
+        write(file, &task.tid, sizeof(task.tid)); // send the tid
+        write(file, &task.priority, sizeof(task.priority)); // send the priority
+        write(file, &task.burst, sizeof(task.burst)); // send the burst
+        write(file, &task.deadline, sizeof(task.deadline)); // send the deadline
+    }
+    sleep(1);
+
+    close(file);
+    unlink(pipefile);
+
+    return 0;
+}
--- Scheduler/schedule_rr_p.c
+++ Scheduler/schedule_rr_p.c
@@ -2,47 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <fcntl.h>
 
 struct node *priority_lists[9];
 
 // add a task to the list
 void add(Task *task)
 {
-   insert(&priority_lists[task->priority -1], task);
-   printf("inserted task %s on list %d\n", task->name, task->priority-1);
+   insert(&priority_lists[task->priority - 1], task);
+   printf("inserted task %s on list %d\n", task->name, task->priority);
 }
 
 void remove_task(Task *task)
 {
-   delete (&priority_lists[task->priority -1], task);
+   delete (&priority_lists[task->priority - 1], task);
 }
 
 // invoke the scheduler
-void schedule() {
-   while (1) {
-      for (int i = 0; i <= 9; i++) {
+void schedule()
+{
+   while (1)
+   {
+      for (int i = 0; i <= 9; i++)
+      {
          struct node *current = priority_lists[i];
-         while (current != NULL) {
+         while (current != NULL)
+         {
             int quantum = 0;
-            while (quantum < QUANTUM && current->task->burst > 0) {
+            while (quantum < QUANTUM && current->task->burst > 0)
+            {
                printf("Running task %s with priority %d with remaining burst %d\n", current->task->name, current->task->priority, current->task->burst);
                current->task->burst--;
                quantum++;
                sleep(1);
             }
-            if (current->task->burst == 0) {
+            if (current->task->burst == 0)
+            {
                struct node *temp = current;
                remove_task(temp->task);
             }
-            if (current->next == NULL) {
+            if (current->next == NULL)
+            {
                current = priority_lists[i];
             }
-            else {
+            else
+            {
                current = current->next;
             }
             // Check if there are any new tasks in a higher priority list
-            for (int j = i - 1; j >= 0; j--) {
-               if (priority_lists[j] != NULL) {
+            for (int j = i - 1; j >= 0; j--)
+            {
+               if (priority_lists[j] != NULL)
+               {
                   i = j; // start from the higher priority list
                }
             }
@@ -73,13 +85,36 @@ Task *create_task(char *name, int tid, int priority, int burst, int deadline)
    return newTask;
 }
 
-int main()
+void *read_tasks(void *arg)
 {
-   printf("Priority-based Round Robin Scheduler\n");
-   printf("=====================================\n");
-   printf("Creating list of tasks\n");
-   initialize_priority_lists();
-   printf("Adding tasks to first list\n");
+   int file;
+   char *pipefile = "/tmp/pipefile";
+
+   file = open(pipefile, O_RDONLY);
+   if (file < 0) {
+      perror("open");
+      return NULL;
+   }
+
+   while (1)
+   {
+      Task *task = (Task *)malloc(sizeof(Task));
+      task->name = (char *)malloc(100); // allocate memory for the name
+      read(file, task->name, 100); // read the name
+      read(file, &task->tid, sizeof(task->tid)); // read the tid
+      read(file, &task->priority, sizeof(task->priority)); // read the priority
+      read(file, &task->burst, sizeof(task->burst)); // read the burst
+      read(file, &task->deadline, sizeof(task->deadline)); // read the deadline
+
+      add(task);
+   }
+
+   close(file);
+   return NULL;
+}
+
+void create_tasks()
+{
    create_task("Task1", 1, 1, 1, 1);
    create_task("Task2", 2, 1, 2, 2);
    create_task("Task3", 3, 1, 4, 4);
@@ -114,8 +149,27 @@ int main()
    create_task("Task44", 44, 5, 8, 8);
    create_task("Task45", 45, 5, 16, 16);
    create_task("Task46", 46, 5, 32, 32);
+}
+
+int main()
+{
+   pthread_t read_tasks_thread;
+   
+   printf("Priority-based Round Robin Scheduler\n");
+   printf("=====================================\n");
+   printf("Creating list of tasks\n");
+
+   initialize_priority_lists();
+
+   printf("Adding tasks to first list\n");
+   create_tasks();
+
+   pthread_create(&read_tasks_thread, NULL, read_tasks, NULL);
 
    printf("\n\nScheduling tasks\n");
    schedule();
+
+   pthread_join(read_tasks_thread, NULL);
+
    return 0;
 }
