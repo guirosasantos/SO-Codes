e102b9552499539eb9de3b7d87064dd7a31f6dfd
guirosasantos <guirosasantos1010@gmail.com>
2024-05-26 22:41:13 -0300

    Adicionando funções nos respectivos .h

--- Scheduler/schedule_rr_p.h
+++ Scheduler/schedule_rr_p.h
@@ -8,3 +8,15 @@ void add(Task *task);
 
 // invoke the scheduler
 void schedule();
+
+// initialize the priority lists
+void initialize_priority_lists();
+
+// create a task
+Task *create_task(char *name, int tid, int priority, int burst, int deadline);
+
+// creates a pipe and create a taks from the pipe
+void *read_tasks(void *arg);
+
+// create a task from a txt file
+void create_tasks_from_file(char *filename);
--- Scheduler/schedulers.h
+++ Scheduler/schedulers.h
@@ -3,7 +3,10 @@
 #include "task.h"
 
 // add a task to the list 
-void add(task task);
+void add(Task *task);
 
 // invoke the scheduler
 void schedule();
+
+// remove task and free memory
+void remove_task(Task *task);
